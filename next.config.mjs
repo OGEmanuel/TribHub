992138e4d42dbb045b3401e7bf2728ada86e4dd9
OGEmanuel <1blacksanta416@gmail.com>
2024-08-04 02:36:27 +0100

    remove:Removed redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://trib-hub-website.vercel.app/",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "https://trib-hub-website.vercel.app/",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
